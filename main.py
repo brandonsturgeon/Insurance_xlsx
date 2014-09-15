032e1c4cfe2a232c69c073d0d43541750605545d
Brandon Sturgeon <sturgeonb4@gmail.com>
2014-09-15 15:40:13 -0500

    Formatting fixes

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def rc_to_ln(r, c):
 # Converts Row,Col,Row,Col range notation to LetterNum:LetterNum notation
 def rc_to_ln_range(r, c, r1, c1):
     letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    return letters[c]+str(r+1)+":"+letters[c1]+str(r+1)
+    return letters[c]+str(r+1)+":"+letters[c1]+str(r1+1)
+
 
 # Main creation class
 class Create():
@@ -67,10 +68,10 @@ class Create():
         page.protect()
 
         # Row/Column sizes
-        page.set_row(0, 35)
+        page.set_row(0, 25)
 
         # Total width should be 175, we merge them later so each needs to be ~half
-        page.set_column(0, 1, 87)
+        page.set_column(0, 1, 25)
 
         # Header
         page.merge_range("A1:B1", "Insurance Policy Info", self.format_01)
@@ -91,35 +92,39 @@ class Create():
         page.merge_range("A1:G1", "Enterprise Units", self.format_01)
         page.merge_range("A3:B3", self.data["policy_info"]["County"], self.format_02)
 
+        # Setting Column Width
+        page.set_column(2, 0, 15)
+        page.set_column(3, 0, 15)
+        page.set_column(4, 0, 15)
+
         # General info
-        page.write(2, 1, data["gen"].keys(), self.format_01)
-        page.write(3, 1, data["gen"].values(), self.format_01)
+        page.write_row(3, 1, data["gen"].keys(), self.format_01)
+        page.write_row(4, 1, data["gen"].values())
 
         # Row counter
         r = 6
         for name, unit in data["units"].iteritems():
             page.write(r, 1, name, self.format_01)
-
             # The general information for this unit
-            page.write(r+1, 2, unit["gen"].keys(), self.format_01)
-            page.write(r+2, 2, unit["gen"].values())
+            page.write_row(r+1, 2, unit["gen"].keys(), self.format_01)
+            page.write_row(r+2, 2, unit["gen"].values())
 
             # ! This will have to change, because dictionaries aren't ordered, which means .keys() won't be ! #
             # Sets our headers for the zone data
             headers = unit["zones"][0].keys()
-            page.write(r+4, 2, headers, self.format_01)
+            page.write_row(r+4, 2, headers, self.format_01)
             r += 5
 
             # Writes the data for each zone in the unit
             for zone in unit["zones"]:
-                page.write(r, 2, zone.values())
+                page.write_row(r, 2, zone.values())
                 r += 1
 
             # Total calculations
             page.write(r, 2, "Totals: ", self.format_03)
             for c in range(4, 6):
-                _range = rc_to_ln_range(r, c, r-len(unit["zones"]), c)
-                page.write_formula(r, c, "=SUM("+_range+")", self.format_03)
+                _range = rc_to_ln_range(r-1, c, r-len(unit["zones"]), c)
+                page.write_formula(r, c, "=SUM("+_range+")", self.unlocked)
             r += 3
 
     # Formats the Optional Units sheet
@@ -136,23 +141,23 @@ class Create():
             page.write(r, 0, name, self.format_01)
 
             # General unit information
-            page.write(r+1, 1, unit["gen"].keys())
-            page.write(r+2, 1, unit["gen"].values())
+            page.write_row(r+1, 1, unit["gen"].keys())
+            page.write_row(r+2, 1, unit["gen"].values())
 
             # Sets the headers for our zone columns
             headers = unit["zones"][0].keys()
-            page.write(r+3, 3, headers, self.format_01)
+            page.write_row(r+3, 3, headers, self.format_01)
 
             r += 5
             # Creates the table by just writing each zone's values as a list
             for zone in unit["zones"]:
-                page.write(r, 1, zone.values())
+                page.write_row(r, 1, zone.values())
                 r += 1
 
             # Total calculation
             page.write(r, 1, "Totals: ", self.format_03)
             for c in range(3, 5):
-                _range = rc_to_ln_range(r, c, r-len(unit["zones"]), c)
+                _range = rc_to_ln_range(r-1, c, r-len(unit["zones"]), c)
                 page.write_formula(r, c, "=SUM("+_range+")", self.format_03)
             r += 3
 
@@ -162,7 +167,11 @@ class Create():
         page.protect()
 
         # Header
-        page.merge_range("A1:J1", "HPP Units", self.format_01)
+        page.merge_range("A1:F1", "HPP Units", self.format_01)
+
+        page.set_column(1, 0, 15)
+        page.set_column(2, 0, 15)
+        page.set_column(3, 0, 15)
 
         # Row counter
         r = 2
@@ -170,24 +179,24 @@ class Create():
             page.write(r, 0, name, self.format_01)
 
             # General unit information
-            page.write(r+1, 1, unit["gen"].keys())
-            page.write(r+2, 1, unit["gen"].values())
+            page.write_row(r+1, 1, unit["gen"].keys(), self.format_01)
+            page.write_row(r+2, 1, unit["gen"].values())
 
             # Sets the headers for our zone columns
             headers = unit["zones"][0].keys()
-            page.write(r+3, 3, headers, self.format_01)
+            page.write_row(r+4, 1, headers, self.format_01)
 
             r += 5
             # Creates the table by just writing each zone's values as a list
             for zone in unit["zones"]:
-                page.write(r, 1, zone.values())
+                page.write_row(r, 1, zone.values())
                 r += 1
 
             # Total calculations
             page.write(r, 1, "Totals: ", self.format_03)
             for c in range(3, 5):
-                _range = rc_to_ln_range(r, c, r-len(unit["zones"]), c)
-                page.write_formula(r, c, "=SUM("+_range+")", self.format_03)
+                _range = rc_to_ln_range(r-1, c, r-len(unit["zones"]), c)
+                page.write_formula(r, c, "=SUM("+_range+")", self.unlocked)
             r += 3
 
 if __name__ == "__main__":
@@ -197,7 +206,26 @@ if __name__ == "__main__":
                  "Units": "optional",
                  "MPCI Coverage": "60%",
                  "Practice": "non_irrigated"},
-            "enterprise_units": {"gen": True, "units": {"unit1": {"gen": {"totalacres": 10}}}}}
+            "enterprise_units": {"gen": {"Total": 493, "Total2": 86700, "Total3": 19720},
+                                 "units": {"unit1": {"gen": {"totalacres": 10,
+                                                             "total2": 70,
+                                                             "total3": 32},
+                                                     "zones": [{"Field-Zone": "zone1",
+                                                                "Acres": 200,
+                                                                "Actual Production": 275000,
+                                                                "Actual Yield": 550}]},
+                                           "unit2": {"gen": {"totalacres": 20,
+                                                             "total2": 50,
+                                                             "total3": 75},
+                                                     "zones": [{"Field-Zone": "zone1",
+                                                                "Acres": 200,
+                                                                "Actual Production": 275000,
+                                                                "Actual Yield": 550}]}}},
+            "hpp_units": {"units": {"unit1": {"gen": {"total acre": 720, "modified APH": 550},
+                                              "zones": [{"Field-Zone": "zone1",
+                                                                "Acres": 200,
+                                                                "Actual Production": 275000,
+                                                                "Actual Yield": 550}]}}}}
     Create("test_file", dic)
 
 
